7a069bb72df0a0a9d57a779fc559bf0524399540
prashanth-cn <prashicn@gmail.com>
2025-01-09 00:02:47 +0530

    feat(plugins): add them back in

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,21 +4,21 @@
 -- See the kickstart.nvim README for more information
 return { -- lazy.nvim
   require 'custom.plugins.themes',
-  -- {
-  --   'folke/noice.nvim',
-  --   event = 'VeryLazy',
-  --   opts = {
-  --     -- add any options here
-  --   },
-  --   dependencies = {
-  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-  --     'MunifTanjim/nui.nvim',
-  --     -- OPTIONAL:
-  --     --   `nvim-notify` is only needed, if you want to use the notification view.
-  --     --   If not available, we use `mini` as the fallback
-  --     'rcarriga/nvim-notify',
-  --   },
-  -- },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
   {
     'MagicDuck/grug-far.nvim',
     opts = { headerMaxWidth = 80 },
@@ -50,58 +50,58 @@ return { -- lazy.nvim
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
-  -- {
-  --   'lukas-reineke/indent-blankline.nvim',
-  --   event = 'VeryLazy',
-  --   opts = function()
-  --     -- Snacks.toggle({
-  --     --   name = "Indention Guides",
-  --     --   get = function()
-  --     --    return require("ibl.config").get_config(0).enabled
-  --     --   end,
-  --     --   set = function(state)
-  --     --     require("ibl").setup_buffer(0, { enabled = state })
-  --     --   end,
-  --     -- }):map("<leader>ug")
-  --     --
-  --     return {
-  --       indent = {
-  --         char = '▏',
-  --         tab_char = '→',
-  --         smart_indent_cap = true,
-  --       },
-  --       scope = {
-  --         enabled = true,
-  --         char = '▎', -- special character for scope lines
-  --         show_start = true,
-  --         show_end = true,
-  --         show_exact_scope = false,
-  --         injected_languages = true,
-  --         -- highlight = rainbow_highlights, -- use rainbow colors for scope
-  --         priority = 1024,
-  --       },
-  --       exclude = {
-  --         filetypes = {
-  --           'Trouble',
-  --           'alpha',
-  --           'dashboard',
-  --           'help',
-  --           'lazy',
-  --           'mason',
-  --           'neo-tree',
-  --           'notify',
-  --           'snacks_dashboard',
-  --           'snacks_notif',
-  --           'snacks_terminal',
-  --           'snacks_win',
-  --           'toggleterm',
-  --           'trouble',
-  --         },
-  --       },
-  --     }
-  --   end,
-  --   main = 'ibl',
-  -- },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'VeryLazy',
+    opts = function()
+      -- Snacks.toggle({
+      --   name = "Indention Guides",
+      --   get = function()
+      --    return require("ibl.config").get_config(0).enabled
+      --   end,
+      --   set = function(state)
+      --     require("ibl").setup_buffer(0, { enabled = state })
+      --   end,
+      -- }):map("<leader>ug")
+      --
+      return {
+        indent = {
+          char = '▏',
+          tab_char = '→',
+          smart_indent_cap = true,
+        },
+        scope = {
+          enabled = true,
+          char = '▎', -- special character for scope lines
+          show_start = true,
+          show_end = true,
+          show_exact_scope = false,
+          injected_languages = true,
+          -- highlight = rainbow_highlights, -- use rainbow colors for scope
+          priority = 1024,
+        },
+        exclude = {
+          filetypes = {
+            'Trouble',
+            'alpha',
+            'dashboard',
+            'help',
+            'lazy',
+            'mason',
+            'neo-tree',
+            'notify',
+            'snacks_dashboard',
+            'snacks_notif',
+            'snacks_terminal',
+            'snacks_win',
+            'toggleterm',
+            'trouble',
+          },
+        },
+      }
+    end,
+    main = 'ibl',
+  },
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
